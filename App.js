46b60e7fa13a8af9b02cdbc92bad9557c282a6d4
Prumme <aurelien23.p@gmail.com>
2023-01-12 12:03:06 +0100

    :tada: modify main

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Alert } from 'react-native';
+
+
+
 
 export default function App() {
+
+  const Separator = () => (
+    <View style={styles.separator} />
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.global}>
+      <Text style={styles.Title}>Bienvenue, nous allons enregistrer votre brossage de dents</Text>
+      <Separator />
+      <Button
+        title='Brosser les dents'
+        color={styles.Button.color}
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
@@ -16,5 +31,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  global: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#606164',
+  },
+  Button: {
+    color: '#00F',
+  },
+  Title: {
+    color: '#FFF',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
